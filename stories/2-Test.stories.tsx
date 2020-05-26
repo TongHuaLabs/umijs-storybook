53e08a617e080925bcec09b1a2c2579c02858b43
Anakorn Kyavatanakij <anakornk@gmail.com>
2020-05-26 23:40:38 +0700

    add antd and antd mobile support

--- stories/2-Test.stories.tsx
+++ stories/2-Test.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Test from './Test';
+import { Button } from 'antd-mobile';
+import { Input } from 'antd';
 
 export default {
   title: 'Test',
@@ -8,3 +10,7 @@ export default {
 };
 
 export const T1 = () => <Test />;
+
+export const T2 = () => <Button type="warning">warning</Button>;
+
+export const T3 = () => <Input placeholder="Basic usage" />;
