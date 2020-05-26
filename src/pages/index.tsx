ddcd57037cebe939f4ba3feb2063e7085beede9e
Anakorn Kyavatanakij <anakornk@gmail.com>
2020-05-26 16:08:18 +0700

    add antd button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './index.less';
+import { Button } from 'antd-mobile';
 
 export default () => {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <Button type="warning">warning</Button>
     </div>
   );
 };
